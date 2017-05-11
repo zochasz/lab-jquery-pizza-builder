95027e8e9f762d7b988aa6bb4d28480e61ee5c08
zocha_sz <zofia.szczudlik@gmail.com>
2017-05-11 22:59:12 +0200

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,20 @@
 // Write your Pizza Builder JavaScript in this file.
+$('.btn-pepperonni').click(function(e) {
+  $('.pep').toggle();
+});
+$('.btn-mushrooms').click(function(e) {
+  $('.mushroom').toggle();
+});
+$('.btn-green-peppers').click(function(e) {
+  $('.green-pepper').toggle();
+});
+$('.btn-crust').click(function(e) {
+  $('.crust').toggleClass('crust-gluten-free');
+});
+$('.btn-sauce').click(function(e) {
+  $('.sauce').toggleClass('sauce-white');
+});
+$('.btn').click(function(e) {
+  $(this).toggleClass('active');
+});
+                                  /* para corregir*/
